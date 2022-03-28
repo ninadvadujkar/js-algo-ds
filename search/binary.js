6152fa603c8f976ae126c0bff11ed7f18f9c3466
Ninad Vadujkar <ninad.vadujkar@vodafoneziggo.com>
2022-03-28 09:49:21 +0200

    add binary search

---
+++ search/binary.js
@@ -0,0 +1,21 @@
+const binarySearch = (array, termToSearch) => {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    const middle = left + Math.round((right - left)/2);
+    const arrayItemToCheck = array[middle];
+    if (arrayItemToCheck === termToSearch) {
+      return middle;
+    }
+    if (termToSearch < arrayItemToCheck) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return -1;
+};
+
+const ret = binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 95);
+console.log(ret);
