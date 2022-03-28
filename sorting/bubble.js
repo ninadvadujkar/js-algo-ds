124a47c5bf0b9198cebca19934192cc0b84fafdc
Ninad Vadujkar <ninad.vadujkar@vodafoneziggo.com>
2022-03-28 14:29:51 +0200

    add bubble sort

---
+++ sorting/bubble.js
@@ -0,0 +1,47 @@
+const bubbleSort = (array) => {
+  const a = array.slice(0);
+  for (let i = a.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      console.log(a, a[j], a[j+1]);
+      if (a[j] > a[j+1]) {
+        swap(a, j, j+1);
+      }
+    }
+    console.log('ONE PASS COMPLETE');
+    console.log('==================');
+  }
+  return a;
+};
+
+const bubbleSortOptimized = (array) => {
+  let noSwaps = false;
+  const a = array.slice(0);
+  for (let i = a.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      console.log(a, a[j], a[j+1]);
+      if (a[j] > a[j+1]) {
+        swap(a, j, j+1);
+        noSwaps = false;
+      }
+    }
+    console.log('ONE PASS COMPLETE');
+    console.log('==================');
+    if (noSwaps) break;
+  }
+  return a;
+};
+
+const swap = (array, index1, index2) => {
+  const temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+};
+
+const ret1 = bubbleSort([45, 23, 67, 22, 56, 8]);
+console.log(ret1);
+
+console.log('\n >>>>>>>>>>>>>> Optimized bubble sort with `noSwaps` >>>>>>>>>>>>>> \n');
+
+const ret2 = bubbleSortOptimized([67, 8, 22, 23, 45, 56]);
+console.log(ret2);
